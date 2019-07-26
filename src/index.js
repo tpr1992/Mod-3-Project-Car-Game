c7573e39defe015dad37138aebacd2205a69b3f0
tpr1992 <terence.pataneronan@gmail.com>
2019-07-26 09:41:34 -0400

    update repo

---
+++ src/index.js
@@ -0,0 +1,363 @@
+
+// -------------------------------Main Variables------------------------------- //
+let usernames = document.querySelector('.usernames').querySelector('ul')
+let userCard = document.querySelector('.user-card')
+let startGameBtn = document.querySelector('.btn')
+let mainTitle = document.querySelector('.main_title')
+let scoreBoard = document.querySelector('.scoreboard')
+let mainTextArea = document.querySelector('.main-text-area')
+let tableRow = document.getElementById('table-row')
+let recordsBtn = document.querySelector('#records-butt')
+let selectedId;
+
+
+// ------------------------------Main Functions-------------------------------- //
+
+// Click on Start Game
+startGameBtn.addEventListener('click', event => {
+  if (mainTextArea.innerHTML.length === 0) {
+  }
+  if (mainTextArea.innerHTML.length > 0) {
+
+    // Fetch users
+    fetch("http://localhost:3000/users")
+    .then(res => res.json())
+    .then((users) => {
+      // Page heading
+      mainTitle.innerHTML = "Choose Your Player"
+      mainTextArea.innerHTML = ""
+      users.forEach(user => {
+        // Fill main text area w/ list of users
+        mainTextArea.innerHTML += createUserList(user)
+        // Create HTML for specific user profiles
+
+        const createUserProfile = (user) => {
+          return `
+          <div align="center">
+          <div class="col-xl">
+          <div class="card" style="width: 28rem;">
+          <img class="card-img-top" src="${user.img_url}" alt="Card image cap">
+          <div class="card-body">
+          <h5 class="card-title">${user.name}</h5>
+          <p class="card-text">${user.tagline}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">$${user.money}</li>
+          <li class="list-group-item">HP: 400</li>
+          <li class="list-group-item">Torque: 350</li>
+          <li class="list-group-item">Weight: 3600</li>
+          </ul>
+          <div id='start-race'>
+          <div class="card-body">
+          <a href="#" data-id="${user.id}" class="btn btn-primary" style="background-color: black; border-color: black" >Start Race</a>
+          <a id="customize-btn" href="#" class="btn btn-primary" style="background-color: black; border-color: black">Customize</a>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>`
+        }
+
+        // Select player by button
+        mainTextArea.addEventListener('click', (event) => {
+          if (event.target.tagName === "A" && event.target.dataset.set == `${user.id}`) {
+            //debugger;
+            mainTextArea.innerHTML = ""
+            mainTextArea.innerHTML = createUserProfile(user)
+            mainTitle.innerHTML = "Peep The Stats"
+            let customizeBtn = document.querySelector("#customize-btn")
+            customizeBtn.addEventListener('click', (event) => {
+              console.log('jfdjkfdkj')
+            })
+            let startBtn = document.querySelector(`#start-race`)
+            startBtn.addEventListener('click', event => {
+              mainTextArea.innerHTML = ""
+              console.log('start race')
+              loop()
+              if (event.target.innerText != 'Start Race') {
+                // noLoop()
+              }
+              else {
+
+                mainTitle.innerHTML = "RACE TIME!"
+                selectedId = event.target.dataset.id
+                loop(selectedId)
+
+
+
+                mainTextArea.innerHTML = `<div align="center" id="reset-btn">
+                <a href="#" class="btn btn-primary btn-md active" style="background-color: black; border-color: black" role="button" aria-pressed="true">Reset Game</a></div>
+                `
+
+                resetBtn = document.querySelector('#reset-btn')
+                resetBtn.addEventListener('click', (event) => {
+                  // redraw()
+                  resetValues()
+                  loop()
+                  console.log('You pressed reset')
+                  // OPPONENTPOSITION = 0
+                  // }, 1000);
+                })
+
+                loop()
+
+
+
+              }
+            })
+          }
+        })
+
+      })
+    })
+  }
+})
+
+// Create table for records list
+const createTable = () => {
+  return`<table class="table table-striped table-dark">
+  <thead>
+  <tr>
+  <th scope="col">User Name</th>
+  <th scope="col">User id</th>
+  <th scope="col">Wins</th>
+  </tr>
+  </thead>
+  <tbody id="table-body">
+  </tbody>
+  </table>`
+}
+
+// Add event listener to Records button
+
+recordsBtn.addEventListener('click', event => {
+  mainTitle.innerHTML = "High Scores"
+
+  fetch("http://localhost:3000/records")
+  .then(res => res.json())
+  .then((allRecords) => {
+    mainTextArea.innerHTML = createTable()
+    let tableBody = document.querySelector('#table-body')
+    // remove()
+    allRecords.forEach(record => {
+      let userId = record.user_id
+      fetch("http://localhost:3000/users")
+      .then(res => res.json())
+      .then((users) => {
+        users.forEach(user => {
+          if (userId === user.id) {
+            tableBody.innerHTML += `<tr data-id="${user.id}">
+            <td>${user.name}</td>
+            <td>${record.id}</td>
+            <td>${record.wins}</td>
+            </tr>`
+          }
+        })
+      })
+    })
+  })
+})
+
+// Create HTML for users list
+const createUserList = (user) => {
+  return `
+  <div align="center">
+  <div class="card-deck" data-set="${user.id}">
+  <div class="card">
+  <div class="w-25 p-3">
+  <img data-set="${user.id}" class="card-img-top" src="${user.img_url}"  alt="Card image cap"> </div>  <div class="card-body">
+  <h5 class="card-title">${user.name}</h5>
+  <p class="card-text">${user.tagline}</p>
+  <div class="select-user-btn" id="select-user-btn">
+  <a href="#" class="btn btn-primary btn-sm active" style="background-color: black; border-color: black" role="button" aria-pressed="true" data-set="${user.id}">Play as ${user.name}</a>
+  </div>
+  <p class="card-text"><small class="text-muted"></small></p>
+  </div>
+  </div>
+  </div>`
+}
+
+
+//
+//
+//
+//
+// //
+// //// -------------------------------Main Variables------------------------------- //
+// let usernames = document.querySelector('.usernames').querySelector('ul')
+// let userCard = document.querySelector('.user-card')
+// let startGameBtn = document.querySelector('.btn')
+// let mainTitle = document.querySelector('.main_title')
+// let scoreBoard = document.querySelector('.scoreboard')
+// let mainTextArea = document.querySelector('.main-text-area')
+// let tableRow = document.getElementById('table-row')
+// let recordsBtn = document.querySelector('#records-butt')
+// let selectedId;
+//
+//
+//
+//
+// // ------------------------------Main Functions-------------------------------- //
+//
+// // Click on Start Game
+// startGameBtn.addEventListener('click', event => {
+//   if (mainTextArea.innerHTML.length === 0) {
+//   }
+//   if (mainTextArea.innerHTML.length > 0) {
+//
+//     // Fetch users
+//     fetch("http://localhost:3000/users")
+//     .then(res => res.json())
+//     .then((users) => {
+//       // Page heading
+//       mainTitle.innerHTML = "Choose Your Player"
+//       mainTextArea.innerHTML = ""
+//       users.forEach(user => {
+//         // Fill main text area w/ list of users
+//         mainTextArea.innerHTML += createUserList(user)
+//         // Create HTML for specific user profiles
+//         const createUserProfile = (user) => {
+//           return `
+//           <div align="center">
+//           <div class="col-xl">
+//           <div class="card" style="width: 28rem;">
+//           <img class="card-img-top" src="${user.img_url}" alt="Card image cap">
+//           <div class="card-body" style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)">
+//           <h5 class="card-title" >${user.name}</h5>
+//           <p class="card-text">${user.tagline}</p>
+//           </div>
+//           <ul class="list-group list-group-flush" >
+//           <li style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)" class="list-group-item">$${user.money}</li>
+//           <li style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)"class="list-group-item">HP: 400</li>
+//           <li style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)"class="list-group-item">Torque: 350</li>
+//           <li style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)"class="list-group-item">Weight: 3600</li>
+//           </ul>
+//           <div id='start-race'>
+//           <div style="background-image: url(https://beautifulpixels.com/wp-content/uploads/2015/11/horizon-chase-bp.jpg)" class="card-body">
+//           <a href="#" data-id="${user.id}" class="btn btn-primary" style="background-color: black; border-color: black" >Start Race</a>
+//           <a id="customize-btn" href="#" class="btn btn-primary" style="background-color: black; border-color: black">Customize</a>
+//           </div>
+//           </div>
+//           </div>
+//           </div>
+//           </div>`
+//         }
+//
+//         // Select player by button
+//         mainTextArea.addEventListener('click', (event) => {
+//           if (event.target.tagName === "A" && event.target.dataset.set == `${user.id}`) {
+//             //debugger;
+//             mainTextArea.innerHTML = ""
+//             mainTextArea.innerHTML = createUserProfile(user)
+//             mainTitle.innerHTML = "Peep The Stats"
+//             let customizeBtn = document.querySelector("#customize-btn")
+//             customizeBtn.addEventListener('click', (event) => {
+//               console.log('jfdjkfdkj')
+//             })
+//             let startBtn = document.querySelector(`#start-race`)
+//             startBtn.addEventListener('click', event => {
+//               mainTextArea.innerHTML = ""
+//               console.log('start race')
+//               loop()
+//               if (event.target.innerText != 'Start Race') {
+//                 // noLoop()
+//               }
+//               else {
+//
+//                 mainTitle.innerHTML = "RACE TIME!"
+//                 selectedId = event.target.dataset.id
+//                 loop(selectedId)
+//
+//
+//
+//                 mainTextArea.innerHTML = `<div align="center" id="reset-btn">
+//                 <a href="#" class="btn btn-primary btn-md active" style="background-color: black; border-color: black" role="button" aria-pressed="true">Reset Game</a></div>
+//                 `
+//
+//                 resetBtn = document.querySelector('#reset-btn')
+//                 resetBtn.addEventListener('click', (event) => {
+//                   // redraw()
+//                   resetValues()
+//                   loop()
+//                   console.log('You pressed reset')
+//                   // OPPONENTPOSITION = 0
+//                   // }, 1000);
+//                 })
+//
+//                 loop()
+//
+//
+//
+//               }
+//             })
+//           }
+//         })
+//
+//       })
+//     })
+//   }
+// })
+//
+// // Create table for records list
+// const createTable = () => {
+//   return`<table class="table table-striped table-dark">
+//   <thead>
+//   <tr>
+//   <th scope="col">User Name</th>
+//   <th scope="col">User id</th>
+//   <th scope="col">Wins</th>
+//   </tr>
+//   </thead>
+//   <tbody id="table-body">
+//   </tbody>
+//   </table>`
+// }
+//
+// // Add event listener to Records button
+//
+// recordsBtn.addEventListener('click', event => {
+//   mainTitle.innerHTML = "High Scores"
+//   fetch("http://localhost:3000/records")
+//   .then(res => res.json())
+//   .then((allRecords) => {
+//     mainTextArea.innerHTML = createTable()
+//     let tableBody = document.querySelector('#table-body')
+//     // remove()
+//     allRecords.forEach(record => {
+//       let userId = record.user_id
+//       fetch("http://localhost:3000/users")
+//       .then(res => res.json())
+//       .then((users) => {
+//         users.forEach(user => {
+//           if (userId === user.id) {
+//             tableBody.innerHTML += `<tr data-id="${user.id}">
+//             <td>${user.name}</td>
+//             <td>${record.id}</td>
+//             <td>${record.wins}</td>
+//             </tr>`
+//           }
+//         })
+//       })
+//     })
+//   })
+// })
+//
+// // Create HTML for users list
+// const createUserList = (user) => {
+//   return `
+//   <div align="center">
+//   <div class="card-deck" data-set="${user.id}">
+//   <div class="card">
+//   <div class="w-25 p-3">
+//   <img data-set="${user.id}" class="card-img-top" src="${user.img_url}"  alt="Card image cap"> </div>  <div class="card-body">
+//   <h5 class="card-title">${user.name}</h5>
+//   <p class="card-text">${user.tagline}</p>
+//
+//   <div class="select-user-btn" id="select-user-btn">
+//   <a href="#" class="btn btn-primary btn-sm active" style="background-color: black; border-color: black" role="button" aria-pressed="true" data-set="${user.id}">Play as ${user.name}</a>
+//   </div>
+//   <p class="card-text"><small class="text-muted"></small></p>
+//   </div>
+//   </div>
+//   </div>`
+// }
---
+++ src/raceGame.js
@@ -0,0 +1,276 @@
+// let car1;
+//
+// function preload(){
+//   OPPONENTPOSITION = 0
+//   PLAYERPOSITION = 0
+//   TESTSPEED = 1.5
+//   OPPPONENTSTESTSPEED = 1.5
+// }
+//
+//
+// function setup() {
+//     createCanvas(windowWidth, 408);
+//     backGround = loadImage('https://i.imgur.com/DXXpRi1.png')
+//     car1 = loadImage('https://images.vexels.com/media/users/3/139450/isolated/preview/ace6caf91a5498b7ba70a50195566614-race-car-speed-racing-by-vexels.png')
+//     car2 = loadImage('https://images.vexels.com/media/users/3/139450/isolated/preview/ace6caf91a5498b7ba70a50195566614-race-car-speed-racing-by-vexels.png')
+//     noLoop()
+//   }
+//
+// console.log('car1', car1)
+//
+// fetch("http://localhost:3000/cars")
+// .then(res => res.json())
+// .then((allCars) => {
+//   allCars.forEach(car => {
+//     let horsepower = `${car.horsepower}`
+//     let torque = `${car.torque}`
+//     console.log(`Horsepower: ${horsepower}`)
+//     console.log(`Torque: ${torque}`)
+//   })
+// })
+//
+//
+//
+//
+// function updateRecords(id, win){
+//   fetch(`http://localhost:3000/records/${id}`, {
+//   method: 'PATCH',
+//   headers: {
+//   "Content-Type": "application/json"
+// },
+//   body: JSON.stringify({wins: win})
+// })
+// }
+//
+// function getRecords(id) {
+//   return fetch(`http://localhost:3000/records/${id}`)
+//     .then(res => res.json())
+// }
+//
+// //
+// // OPPONENTPOSITION = 0
+// // PLAYERPOSITION = 0
+//
+//
+// document.addEventListener("keydown", function(e) {
+//   e = e || window.event;
+//   if (e.keyCode == "39") {
+//     moveCar(700)
+//     console.log('right')
+//   }
+//   else if (e.keyCode == "40") {
+//     console.log("down");
+//   }
+// });
+//
+//
+//
+// // Set power of car
+// const moveCar = (horsepower) => {
+//   if (horsepower < 200) {
+//     PLAYERPOSITION += 17
+//   }
+//   else if (horsepower >= 200 && horsepower < 350) {
+//     PLAYERPOSITION += 28
+//   }
+//   else if (horsepower >= 350 && horsepower < 500) {
+//     PLAYERPOSITION += 35
+//   }
+//   else if (horsepower >= 500) {
+//     PLAYERPOSITION += 50
+//   }
+//   else if (horsepower >= 700) {
+//     PLAYERPOSITION += 70
+//   }
+// }
+//
+//
+// function draw() {
+//     background(backGround);
+//     fill(255);
+//     noStroke();
+//     textAlign(CENTER, CENTER);
+//     // text("use arrow keys, or SPACE to stop",
+//     // width/2, height*0.67);
+//     image(car1, OPPONENTPOSITION, 215, car1.width/6, car1.height/6)
+//     OPPONENTPOSITION += 10
+//     image(car2, PLAYERPOSITION, 270, car2.width/6, car2.height/6)
+//     // drawWords(width * 0.5)
+//     if (OPPONENTPOSITION > window.innerWidth){
+//       textSize(75)
+//       textAlign(CENTER, CENTER)
+//       stroke('#222222');
+//       strokeWeight(4);
+//       text("YOU LOST!",windowWidth / 2, 200)
+//       noLoop()
+//       }
+//     else if (PLAYERPOSITION > window.innerWidth) {
+//       textSize(75)
+//       textAlign(CENTER, CENTER)
+//       stroke('#222222');
+//       strokeWeight(4);
+//       text("YOU WON!", windowWidth / 2, 200)
+//
+//       getRecords(selectedId).then(record => {
+//         console.log('FUCKME', record);
+//         console.log('INCREASE')
+//         record.wins++;
+//         updateRecords(selectedId, record.wins)
+//
+//       }); // returns a record object
+//               noLoop()
+//       }
+// }
+//
+//
+// function resetValues(){
+//   OPPONENTPOSITION = 0
+//   PLAYERPOSITION = 0
+//   TESTSPEED = 1.5
+//   OPPPONENTSTESTSPEED = 1.5
+//
+
+// --------------------------------------------------------------------
+
+let car1;
+
+function preload(){
+  OPPONENTPOSITION = 0
+  PLAYERPOSITION = 0
+  TESTSPEED = 1.5
+  OPPONENTTESTSPEED = 1.3
+  DRAW = 0
+}
+
+function setup() {
+  createCanvas(windowWidth, 408);
+  backGround = loadImage('https://i.imgur.com/DXXpRi1.png')
+  car1 = loadImage('https://images.vexels.com/media/users/3/139450/isolated/preview/ace6caf91a5498b7ba70a50195566614-race-car-speed-racing-by-vexels.png')
+  console.log(car1);
+  car2 = loadImage('https://images.vexels.com/media/users/3/139450/isolated/preview/ace6caf91a5498b7ba70a50195566614-race-car-speed-racing-by-vexels.png')
+  noLoop()
+}
+
+function updateRecords(id, win){
+  fetch(`http://localhost:3000/records/${id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({wins: win})
+  })
+}
+
+function getRecords(id) {
+  return fetch(`http://localhost:3000/records/${id}`)
+  .then(res => res.json())
+}
+document.addEventListener("keydown", function(e) {
+  // debugger
+  e = e || window.event;
+  if (e.keyCode == "39") {
+    // moveCar(300)
+    // test algo
+    moveCar('test')
+    console.log('right')
+  }
+  else if (e.keyCode == "40") {
+    console.log("down");
+  }
+});
+
+// Set power of car
+const moveCar = (horsepower) => {
+  if (horsepower < 200) {
+    PLAYERPOSITION += 17
+  }
+  else if (horsepower >= 200 && horsepower < 350) {
+    PLAYERPOSITION += 28
+  }
+  else if (horsepower >= 350 && horsepower < 500) {
+    PLAYERPOSITION += 35
+  }
+  else if (horsepower >= 500) {
+    PLAYERPOSITION += 50
+  }
+  else if (horsepower >= 700) {
+    PLAYERPOSITION += 70
+  }
+  else if (horsepower === 'test') {
+    console.log('width', PLAYERPOSITION, window.innerWidth/2)
+    if (PLAYERPOSITION > (window.innerWidth / 2)) {
+      console.log('window', window.innerWidth)
+      TESTSPEED -= 2.5;
+    }
+    else if (PLAYERPOSITION > 0 ) {
+      console.log('A;DSKFJASL;DFJ')
+      TESTSPEED *= 1.13;
+    }
+    PLAYERPOSITION += TESTSPEED
+  }
+}
+
+function draw() {
+  console.log("drawwwwwwww")
+  // if (true) {
+  background(backGround);
+  fill(255);
+  noStroke();
+  textAlign(CENTER, CENTER);
+  image(car1, OPPONENTPOSITION, 215, car1.width/6, car1.height/6)
+  OPPONENTTESTSPEED *= 1.03
+  OPPONENTPOSITION += OPPONENTTESTSPEED
+  image(car2, PLAYERPOSITION, 270, car2.width/6, car2.height/6)
+  // drawWords(width * 0.5)
+  if (OPPONENTPOSITION > window.innerWidth){
+    textSize(75)
+    textAlign(CENTER, CENTER)
+    stroke('#222222');
+    strokeWeight(4);
+    text("YOU LOST!",windowWidth / 2, 150)
+    textSize(30)
+
+    text("Click again to restart", windowWidth/2, 260)
+    noLoop()
+  }
+  else if (PLAYERPOSITION > window.innerWidth) {
+    textSize(75)
+    textAlign(CENTER, CENTER)
+    stroke('#222222');
+    strokeWeight(4);
+    text("YOU WON!", windowWidth / 2, 200)
+    textSize(30)
+    text("Click again to restart", windowWidth/2, 260)
+    textSize(25)
+    text(`Top Speed: ${parseInt(TESTSPEED) * 2}MPH`, windowWidth/2, 300)
+
+    // -------------------------
+    getRecords(selectedId).then(record => {
+      console.log('FUCKME', record);
+      console.log('INCREASE')
+      record.wins++;
+      updateRecords(selectedId, record.wins)
+    })
+    // -----------------------------------
+    noLoop()
+    resetValues()
+  }
+}
+function mousePressed(event) {
+  if (event.target.tagName === "CANVAS") {
+    console.log(event)
+    OPPONENTPOSITION = 0
+    PLAYERPOSITION = 0
+    TESTSPEED = 1.5
+    OPPONENTTESTSPEED = 1.5
+    noLoop()
+    loop()
+
+  }}
+
+  function resetValues(){
+    OPPONENTPOSITION = 0
+    PLAYERPOSITION = 0
+    TESTSPEED = 1.5
+    OPPONENTTESTSPEED = 1.5
+  }
